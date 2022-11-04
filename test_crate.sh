e6d79a111b453a4170b28d51e0acb5c47020ab6e
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-11-04 23:10:39 +0100

    hyper

--- test_crate.sh
+++ test_crate.sh
@@ -1,6 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 CRATE="${1:-hyper}"
 TOOLCHAIN="${TOOLCHAIN:-dev}"
 
 echo "Testing $CRATE +$TOOLCHAIN"
 
-cd "$CRATE" && cargo "+$TOOLCHAIN" build --features=full
+cd $CRATE
+
+function build() {
+    (cd "$CRATE" && RUSTFLAGS='-Zincremental-verify-ich' cargo "+$TOOLCHAIN" build --features=full)
+}
+
+build
+
+for patch in ./patches/* ; do
+    echo "Applying $patch..."
+    
+    (cd "$CRATE" && git apply "../$patch")
+    
+    build
+    
+    (cd "$CRATE" && git checkout HEAD .)
+    
+    build
+done
