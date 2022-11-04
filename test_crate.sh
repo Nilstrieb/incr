bf79b6344a0040ecc5788b7bf4e91be2ecc5f8f6
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-11-05 00:35:55 +0100

    testing

--- test_crate.sh
+++ test_crate.sh
@@ -9,7 +9,7 @@ echo "Testing $CRATE +$TOOLCHAIN"
 cd $CRATE
 
 function build() {
-    (cd "$CRATE" && cargo "+$TOOLCHAIN" rustc --features=full --verbose -- -Zincremental-verify-ich -Zquery-dep-graph -Zdump-dep-graph)
+    (cd "$CRATE" && perf record cargo "+$TOOLCHAIN" rustc --features=full --verbose -- -Zincremental-verify-ich -Zquery-dep-graph -Zdump-dep-graph --emit=metadata)
 }
 
 for patch in ./patches/* ; do
@@ -21,12 +21,14 @@ for patch in ./patches/* ; do
     (cd "$CRATE" && git apply "../$patch")
     
     export RUST_DEP_GRAPH="../dep_graph1"
+    export RUSTC_RED_NODES_PATH="../red1"
     build
     
     (cd "$CRATE" && git checkout HEAD .)
     
     echo "Reverting $patch..."
     
+    export RUSTC_RED_NODES_PATH="../red2"
     export RUST_DEP_GRAPH="../dep_graph2"
     build
 done
